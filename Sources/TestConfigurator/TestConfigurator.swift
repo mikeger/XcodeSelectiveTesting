532b6f35a8724b5678331372148bcc50a47b14e4
Mike Gerasimenko <mihail@gerasimenko.me>
2023-05-31 23:35:18 +0200

    Enable tests

--- Sources/TestConfigurator/TestConfigurator.swift
+++ Sources/TestConfigurator/TestConfigurator.swift
@@ -7,11 +7,32 @@ import PathKit
 import Workspace
 import Logger
 
-public func enableTests(at testPlanPath: Path, targetsToTest: Set<TargetIdentity>) throws {
-    
-    let testPlan = try TestPlanHelper.readTestPlan(filePath: testPlanPath.string)
+extension TestPlanHelper {
+    static func updateSelectedTestTargets(testPlan: inout TestPlanModel, with targets: Set<TargetIdentity>) {
+        checkForTestTargets(testPlan: testPlan)
         
-//    TestPlanHelper.updateSelectedTests(testPlan: &testPlan, with: tests, override: false)
+        let targetsToTest = Set(targets.map { target in
+            switch target {
+            case .swiftPackage(_, let name):
+                return name
+            case .target(_, let name):
+                return name
+            }
+        })
+        
+        for (index, _) in testPlan.testTargets.enumerated() {
+            if targetsToTest.contains(testPlan.testTargets[index].target.name) {
+                testPlan.testTargets[index].selectedTests = []
+                testPlan.testTargets[index].skippedTests = []
+            }
+        }
+    }
+}
+
+public func enableTests(at testPlanPath: Path, targetsToTest: Set<TargetIdentity>) throws {
+    var testPlan = try TestPlanHelper.readTestPlan(filePath: testPlanPath.string)
+        
+    TestPlanHelper.updateSelectedTestTargets(testPlan: &testPlan, with: targetsToTest)
     
     try TestPlanHelper.writeTestPlan(testPlan, filePath: testPlanPath.string)
 }
