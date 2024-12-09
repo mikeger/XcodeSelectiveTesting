5dc3c7c1906972f3b90e85a8e163ee3ed66a435f
sam woolf <sam.woolf@gmail.com>
2024-12-09 12:23:29 +0000

    change path to gitRoot (#43)

--- Sources/Git/Git+Changeset.swift
+++ Sources/Git/Git+Changeset.swift
@@ -23,7 +23,7 @@ public extension Git {
             currentBranch = "HEAD"
         }
 
-        let changes = try Shell.execOrFail("cd \(path) && git diff '\(baseBranch)'..'\(currentBranch)' --name-only")
+        let changes = try Shell.execOrFail("cd \(gitRoot) && git diff '\(baseBranch)'..'\(currentBranch)' --name-only")
         let changesTrimmed = changes.trimmingCharacters(in: .whitespacesAndNewlines)
 
         guard !changesTrimmed.isEmpty else {
