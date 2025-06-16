513b7cf001931a2a4a22bb82acd77f9ee76b595e
fxwx23 <fxwx23@gmail.com>
2025-06-16 15:45:46 +0900

    Use `swiftLanguageMode(.v5)` due to lack of concurrency-safety

--- Package.swift
+++ Package.swift
@@ -16,36 +16,61 @@ let products: [PackageDescription.Product] = [
 let targets: [PackageDescription.Target] = [
     .executableTarget(
         name: "xcode-selective-test",
-        dependencies: ["SelectiveTestingCore",
-                       .product(name: "ArgumentParser", package: "swift-argument-parser")]
+        dependencies: [
+            "SelectiveTestingCore",
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        ],
+        swiftSettings: [.swiftLanguageMode(.v5)]
     ),
-    .target(name: "SelectiveTestingCore",
-            dependencies: ["DependencyCalculator",
-                           "TestConfigurator",
-                           "Git",
-                           "PathKit",
-                           "Rainbow",
-                           "Yams"]),
-    .target(name: "DependencyCalculator",
-            dependencies: ["Workspace", "PathKit", "SelectiveTestLogger", "Git"]),
-    .target(name: "TestConfigurator",
-            dependencies: ["Workspace", "PathKit", "SelectiveTestLogger"]),
-    .target(name: "Workspace",
-            dependencies: ["XcodeProj", "SelectiveTestLogger"]),
-    .target(name: "Git",
-            dependencies: ["SelectiveTestShell", "SelectiveTestLogger", "PathKit"]),
-    .target(name: "SelectiveTestLogger",
-            dependencies: ["Rainbow"]),
-    .target(name: "SelectiveTestShell"),
+    .target(
+        name: "SelectiveTestingCore",
+        dependencies: [
+            "DependencyCalculator",
+            "TestConfigurator",
+            "Git",
+            "PathKit",
+            "Rainbow",
+            "Yams",
+        ],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(
+        name: "DependencyCalculator",
+        dependencies: ["Workspace", "PathKit", "SelectiveTestLogger", "Git"],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(
+        name: "TestConfigurator",
+        dependencies: ["Workspace", "PathKit", "SelectiveTestLogger"],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(
+        name: "Workspace",
+        dependencies: ["XcodeProj", "SelectiveTestLogger"],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(
+        name: "Git",
+        dependencies: ["SelectiveTestShell", "SelectiveTestLogger", "PathKit"],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(
+        name: "SelectiveTestLogger",
+        dependencies: ["Rainbow"],
+        swiftSettings: [.swiftLanguageMode(.v5)]
+    ),
+    .target(name: "SelectiveTestShell", swiftSettings: [.swiftLanguageMode(.v5)]),
     .testTarget(
         name: "SelectiveTestingTests",
         dependencies: ["xcode-selective-test", "PathKit"],
-        resources: [.copy("ExampleProject")]
+        resources: [.copy("ExampleProject")],
+        swiftSettings: [.swiftLanguageMode(.v5)]
     ),
     .testTarget(
         name: "DependencyCalculatorTests",
         dependencies: ["DependencyCalculator", "Workspace", "PathKit", "SelectiveTestingCore"],
-        resources: [.copy("ExamplePackages")]
+        resources: [.copy("ExamplePackages")],
+        swiftSettings: [.swiftLanguageMode(.v5)]
     ),
     .plugin(
         name: "SelectiveTestingPlugin",
@@ -55,7 +80,7 @@ let targets: [PackageDescription.Target] = [
                 description: "Configure test plan for current changeset"
             ),
             permissions: [
-                .writeToPackageDirectory(reason: "Update test plan file"),
+                .writeToPackageDirectory(reason: "Update test plan file")
             ]
         ),
         dependencies: ["xcode-selective-test"]
@@ -65,12 +90,14 @@ let targets: [PackageDescription.Target] = [
 let package = Package(
     name: "XcodeSelectiveTesting",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v12)
     ],
     products: products,
     dependencies: [
         .package(url: "https://github.com/tuist/XcodeProj.git", .upToNextMajor(from: "9.0.2")),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.0")),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.0")
+        ),
         .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.5"),
